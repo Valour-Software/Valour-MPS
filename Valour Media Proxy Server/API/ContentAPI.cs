f6b10dac5f7611932c67728c5ae5ee1259354819
Kobe Albright <kobe.albright@gmail.com>
2021-11-20 22:58:11 -0600

    Another one bites the dustgit add -A .git add -A .

--- Valour Media Proxy Server/API/ContentAPI.cs
+++ Valour Media Proxy Server/API/ContentAPI.cs
@@ -108,6 +108,9 @@ namespace Valour.MPS.API
             // Handle partial requests
             if (range)
             {
+                if (re is null)
+                    re = len - 1;
+
                 context.Response.Headers.Add("Content-Range", $"bytes {rs}-{re}/{len}");
                 context.Response.Headers.Add("Content-Length", bytes.Length.ToString());
                 context.Response.StatusCode = 206;
