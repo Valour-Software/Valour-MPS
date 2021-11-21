8151e85822f3525004b646b693a0961989e0c372
Kobe Albright <kobe.albright@gmail.com>
2021-11-20 21:45:10 -0600

    Support cringe range requests

--- Valour Media Proxy Server/API/ContentAPI.cs
+++ Valour Media Proxy Server/API/ContentAPI.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
@@ -20,8 +21,59 @@ namespace Valour.MPS.API
         }
 
         private static async Task GetRoute(IMemoryCache cache, HttpContext context, MediaDB db,
-             string type, string id, ulong user_id)
+             string type, string id, ulong user_id,
+             [FromHeader] string range_header)
         {
+            bool range = false;
+            int rs = 0;
+            int re = 0;
+
+            if (!string.IsNullOrWhiteSpace(range_header))
+            {
+                range = true;
+
+                if (range_header.Length < 9)
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Malformed range header");
+                    return;
+                }
+
+
+                var inter = range_header.Substring(0, 6);
+
+                string[] vals = inter.Split('-');
+
+                if (vals.Length < 2)
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Malformed range header");
+                    return;
+                }
+
+                bool parsed = false;
+
+                // parse start
+                parsed = int.TryParse(vals[0], out rs);
+
+                if (!parsed)
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Malformed range header");
+                    return;
+                }
+
+                parsed = int.TryParse(vals[1], out re);
+
+                if (!parsed)
+                {
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync("Malformed range header");
+                    return;
+                }
+            }
+
+
             if (string.IsNullOrWhiteSpace(type))
             {
                 context.Response.StatusCode = 400;
@@ -44,7 +96,7 @@ namespace Valour.MPS.API
 
             type = type.ToLower();
 
-            byte[] bytes = await GetBytes(cache, (string)id, type, user_id);
+            byte[] bytes = await GetBytes(cache, (string)id, type, user_id, range, rs, re);
 
             if (bytes == null)
             {
@@ -66,9 +118,15 @@ namespace Valour.MPS.API
             if (meta.Length > 1)
                 ext = meta[1];
 
+            // Handle partial requests
+            if (range)
+            {
+                context.Response.StatusCode = 206;
+            }
+
+
             context.Response.ContentType = meta[0];
-            await context.Response.BodyWriter.WriteAsync(bytes);
-            
+            await context.Response.BodyWriter.WriteAsync(bytes);      
         }
 
         ////////////////////
@@ -96,32 +154,65 @@ namespace Valour.MPS.API
             return meta;
         }
 
-        private static async Task<byte[]> GetBytes(IMemoryCache cache, string id, string type, ulong user_id)
+        private static async Task<byte[]> GetBytes(IMemoryCache cache, string id, string type, ulong user_id,
+            // rs is range start, re is range end
+            bool range = false, int rs = 0, int re = 0)
         {
-            byte[] bytes;
+            byte[] bytes = null;
 
-            if (!cache.TryGetValue(id, out bytes))
+            if (range)
             {
-                // Check for user file
-                string user_path = $"../Content/users/{user_id}/{type}/{id}";
-
-                if (!File.Exists(user_path))
+                if (!cache.TryGetValue(id + $"-range-{rs}-{re}", out bytes))
                 {
-                    return null;
+                    bytes = new byte[re - rs];
+
+                    // Check for user file
+                    string user_path = $"../Content/users/{user_id}/{type}/{id}";
+
+                    if (!File.Exists(user_path))
+                    {
+                        return null;
+                    }
+
+                    // Get root file
+                    var root_path = $"../Content/{type}/{id}";
+
+                    if (!File.Exists(root_path))
+                    {
+                        return null;
+                    }
+
+                    var stream = File.OpenRead(root_path);
+
+                    await stream.ReadAsync(bytes, rs, re - re);
+
+                    cache.Set(id + $"-range-{rs}-{re}", bytes);
                 }
-
-                // Get root file
-                var root_path = $"../Content/{type}/{id}";
-
-                if (!File.Exists(root_path))
-                {
-                    return null;
-                }
-
-                bytes = await File.ReadAllBytesAsync(root_path);
-
-                cache.Set(id, bytes);
             }
+            else
+            {
+                if (!cache.TryGetValue(id, out bytes))
+                {
+                    // Check for user file
+                    string user_path = $"../Content/users/{user_id}/{type}/{id}";
+
+                    if (!File.Exists(user_path))
+                    {
+                        return null;
+                    }
+
+                    // Get root file
+                    var root_path = $"../Content/{type}/{id}";
+
+                    if (!File.Exists(root_path))
+                    {
+                        return null;
+                    }
+
+                    bytes = await File.ReadAllBytesAsync(root_path);
+                    cache.Set(id, bytes);
+                }
+            }    
 
             return bytes;
         }
